74ca2141a1a84a2cac663308d37739655f1e6445
Lud Ribeiro <ludmar.ribeiro@gmail.com>
2020-02-17 01:12:05 -0300

    feat: initial implementation for the spring application

---
+++ src/main/java/com/lud/challenge/kodo/App.java
@@ -0,0 +1,22 @@
+package com.lud.challenge.kodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Application object
+ *
+ * @author Lud Ribeiro
+ */
+@SpringBootApplication
+public class App {
+
+    /**
+     * Application main method
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
